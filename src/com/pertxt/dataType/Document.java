b0f9400a9d24592e7f556df1e2f885d32e1d0fd9
mhdehghan <mh.dehghan@ut.ac.ir>
2018-05-06 21:42:07 +0430

    Pre-alpha version of PerTxT

---
+++ src/com/pertxt/dataType/Document.java
@@ -0,0 +1,128 @@
+package com.pertxt.dataType;
+
+import java.util.LinkedList;
+
+
+public class Document extends LinkedList<Sentence>
+{
+    /*private LinkedList<Sentence> sentences=new LinkedList<>();*/
+
+
+    //Copy constructor
+    public Document(Document document) {
+        for (Sentence sentence : document) {
+            Sentence sent = new Sentence(sentence);
+            this.add(sent);
+        }
+    }
+    public Document()
+    {
+
+    }
+    public Document(Sentence[] sentences) {
+        for (Sentence sentence : sentences) {
+            if (sentence != null) {
+                this.add(sentence);
+            }
+        }
+    }
+
+
+    public void add(String sentence)
+    {
+        this.add(new Sentence(sentence));
+    }
+
+    /*public void add(Sentence sentence){sentences.add(sentence);}*/
+
+    /*public Sentence get(int i)
+    {
+        if(i>this.size() || i<0)return null;
+        return this.get(i);
+    }*/
+    public void replaceSentence(int i,String sentence)
+    {
+        if(i>this.size() || i<0)return;
+        this.set(i,new Sentence(sentence));
+    }
+    public void replaceSentence(int i,Sentence sentence)
+    {
+        if(i>this.size() || i<0)return;
+        this.set(i,sentence);
+    }
+
+
+    /*public void remove(int i)
+    {
+        if(i>sentences.size() || i<0)return;
+        sentences.remove(i);
+    }*/
+    /*public int length()
+    {
+        return this.size();
+    }*/
+
+/*    //I think this function dose not work correctly!
+    public boolean existSentence(String sentence)
+    {
+        if(sentences.contains(new Sentence(sentence)))return true;
+        return false;
+    }
+    //I think this function dose not work correctly!
+    public boolean existSentence(Sentence sentence)
+    {
+        if(sentences.contains(sentence))return true;
+        return false;
+    }*/
+
+    //Todo: fix this. It should return just posTags not words!
+    public String posTagsToString()
+    {
+        String result="";
+        for (Sentence tokens : this) {
+            result += tokens.toStringWordPOS(" ") + " ";
+        }
+        result=result.trim();
+        return result;
+    }
+    public String[] posTagsToArray()
+    {
+        String result="";
+        for (Sentence tokens : this) {
+            result += tokens.toStringPOS() + " ";
+        }
+        result=result.trim();
+        return result.split(" ");
+    }
+
+    public String toString()
+    {
+        String result="";
+        for (Sentence tokens : this) {
+            result += tokens.toString() + " ";
+        }
+        result=result.trim();
+        return result;
+    }
+    public String[] toArrayString() {
+        String result = "";
+        for (Sentence tokens : this) {
+            result += tokens.toString() + "\n";
+        }
+        result = result.trim();
+        return result.split("\n");
+
+    }
+
+    public void setDocument(Sentence[] sentences)
+    {
+        this.clear();
+        for (Sentence sentence : sentences) {
+            if (sentence != null) {
+                this.add(sentence);
+            }
+        }
+    }
+
+
+}
---
+++ src/com/pertxt/dataType/Sentence.java
@@ -0,0 +1,172 @@
+package com.pertxt.dataType;
+
+import com.pertxt.dataType.Token;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class Sentence extends LinkedList<Token> {
+
+  /*private LinkedList<Token> tokens=new LinkedList<>();*/
+//    private HashMap<String,Integer> termFrq;
+  //private LinkedList<String> words=new LinkedList<>();
+  //private LinkedList<String> posTags=new LinkedList<>();
+  private int cntWords = 0;
+
+
+  //Copy constructor
+  Sentence(Sentence sentence) {
+    for (Token token : sentence) {
+      Token tkn = new Token(token);
+      this.add(tkn);
+    }
+  }
+
+  public Sentence(String sent) {
+    for (String word : sent.split(" ")) {
+      if (!word.trim().equals("")) {
+        this.add(new Token(word));
+      }
+    }
+  }
+
+  public Sentence(Token[] tokens) {
+    for (Token token : tokens) {
+      if (token != null) {
+        this.add(token);
+      }
+    }
+  }
+
+  public boolean add(String token) {
+    return this.add(new Token(token));
+  }
+
+  /*    @Deprecated
+      public void add(Token token){tokens.add(token);}*/
+    /*public Token get(int i)
+    {
+        if(i>tokens.size() || i<0)return null;
+        return tokens.get(i);
+    }*/
+  public void replaceToken(int i, String token) {
+    if (i > this.size()) {
+      return;
+    }
+    this.set(i, new Token(token));
+  }
+
+  public void replaceToken(int i, Token token) {
+    if (i > this.size()) {
+      return;
+    }
+    this.set(i, token);
+  }
+
+    /*@Deprecated
+    public void remove(int i)
+    {
+        if(i>tokens.size())return;
+        tokens.remove(i);
+    }*/
+/*    public int length()
+    {
+        this.size();
+    }*/
+
+  //I think this function dose not work correctly!
+  public boolean existWord(String token) {
+    return this.contains(new Token(token));
+  }
+
+  //I think this function dose not work correctly!
+  public boolean existWord(Token token) {
+    return this.contains(token);
+  }
+
+  public String toString() {
+    String result = "";
+    for (Token token : this) {
+      result += token.toString() + " ";
+    }
+    result = result.trim();
+    return result;
+  }
+
+  public String toStringWordPOS(String separator) {
+    String result = "";
+    for (Token token : this) {
+      result += token.toString() + separator + token.getPosTag() + " ";
+    }
+    result = result.trim();
+    return result;
+  }
+
+  public String toStringPOS() {
+    String result = "";
+    for (Token token : this) {
+      result += token.getPosTag() + " ";
+    }
+    result = result.trim();
+    return result;
+  }
+
+
+  public List<String> toListPOS() {
+    List<String> pos = new ArrayList<>(this.size());
+    for (int i = 0; i < this.size(); i++) {
+      pos.add(this.get(i).getPosTag());
+    }
+    return pos;
+  }
+
+  public void clearSentence() {
+    this.clear();
+  }
+
+  public void setSentence(String sent) {
+    this.clear();
+    for (String word : sent.split(" ")) {
+      this.add(new Token(word));
+    }
+  }
+
+  public void setSentence(Token[] sent) {
+    this.clear();
+  }
+
+  public void setPOSTags(String[] posTags) {
+    if (posTags.length != this.size()) {
+      System.out.println("Unmatched length!");
+      System.out.println(
+          "The sentence length is: " + this.size() + "\nThe input length is: " + posTags.length);
+      return;
+    }
+    for (int i = 0; i < posTags.length; i++) {
+      this.get(i).setPosTag(posTags[i]);
+    }
+  }
+
+  public void setPOSTags(List<String> posTags) {
+    if (posTags.size() != this.size()) {
+      System.out.println("Unmatched length!");
+      System.out.println(
+          "The sentence length is: " + this.size() + "\nThe input length is: " + posTags.size());
+      return;
+    }
+    for (int i = 0; i < posTags.size(); i++) {
+      this.get(i).setPosTag(posTags.get(i));
+    }
+  }
+
+  public LinkedList<String> ToLinkedListString() {
+    return null;
+  }
+
+  public String[] ToArrayString() {
+    return null;
+  }
+
+
+}

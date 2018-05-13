ca18c90b695558fa921f6b8ab8edd585991b8b67
mhdehghan <mh.dehghan@ut.ac.ir>
2018-05-13 21:26:05 +0430

    Pre-alpha version of PerTxT

--- src/com/pertxt/Processor.java
+++ src/com/pertxt/Processor.java
@@ -14,11 +14,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Processor {
+
   public static void main(String[] args) {
 
     Normalizer normalizer = new Normalizer();
-    WordTokenizer wordTokenizer=new WordTokenizer();
-    POSTagger posTagger=new POSTagger();
+    WordTokenizer wordTokenizer = new WordTokenizer();
+    POSTagger posTagger = new POSTagger();
 
     System.out.println("PerTxT: Pre-alpha version!");
     String inSentence = "";
@@ -26,25 +27,26 @@ public class Processor {
       System.out.println("Enter your sentence: ");
       java.util.Scanner scanner = new java.util.Scanner(System.in);
       inSentence = scanner.nextLine();
-      Sentence sentence=new Sentence(inSentence);
+      Sentence sentence = new Sentence(inSentence);
 
-      if (inSentence.length() == 0) break;
+      if (inSentence.length() == 0) {
+        break;
+      }
       normalizer.process(sentence);
       System.out.println("Normalizer: Your sentence is: " + sentence.toString());
 
-      SentenceTokenizer sentenceTokenizer=new SentenceTokenizer();
-      Document document=sentenceTokenizer.process(sentence);
-      System.out.println("Number of sentences: "+document.size());
+      SentenceTokenizer sentenceTokenizer = new SentenceTokenizer();
+      Document document = sentenceTokenizer.process(sentence);
+      System.out.println("Number of sentences: " + document.size());
 
-      for (int i = 0; i <document.size() ; i++) {
+      for (int i = 0; i < document.size(); i++) {
         //System.out.println(document.get(i).toString());
         wordTokenizer.process(document.get(i));
-        System.out.println("Sentence number "+(i+1)+" is: " + document.get(i).toString());
+        System.out.println("Sentence number " + (i + 1) + " is: " + document.get(i).toString());
         posTagger.process(document.get(i));
-        System.out.println("Tagged sentence "+(i+1)+" is: " + document.get(i).toStringWordPOS("-"));
+        System.out
+            .println("Tagged sentence " + (i + 1) + " is: " + document.get(i).toStringWordPOS("-"));
       }
-
-
 
       wordTokenizer.process(sentence);
       System.out.println("WordTokenizer: Your sentence is: " + sentence.toString());
